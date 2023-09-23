b3e42141848b9e6e01b90dc0c1f50e6756f8f370
TxemaN <txalcala@hotmail.com>
2023-09-23 12:51:25 +0200

    puerto y mas instalaciones

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require("express")
 const cors = require('cors')
+var bodyParser = require('body-parser')
+
+const app = express();
 
 require('dotenv').config();
 
+const port = process.env.PORT || 3000;
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.listen(port, () => {
+    console.log(`Servidor ON en ${port}`)
+})
+  

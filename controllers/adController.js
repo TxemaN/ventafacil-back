39b908d7319a6fa3f87e51775328aac2b47da0c6
TxemaN <txalcala@hotmail.com>
2023-09-23 16:35:40 +0200

    get all ads

--- controllers/adController.js
+++ controllers/adController.js
@@ -1,6 +1,28 @@
 const { getAllAds } = require('../models/adsModel');
 
 
+const getAds = async (req, res) => {
+    //   
+    let data;
+    try {
+        const { } = req.body;
+        
+            data = await getAllAds();
+
+
+        res.status(200).json({
+            ok: true,
+            data
+        });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: true,
+            msg: 'Contacte el adm'
+        });
+
+    }
+};
 
 
 
--- models/adsModel.js
+++ models/adsModel.js
@@ -1,8 +1,19 @@
 const querieEntries = require('./queries.js')
-
+const {pool} = require('../utils/connectPool')
 
 const getAllAds= async ()=> {
-    
+  let client , result;
+  try {
+      client = await pool.connect();
+      const data = await client.query(querieAds.byAll);
+      result = await data.rows
+  } catch (error) {
+      console.log(error);
+      throw new Error('error')
+  } finally {
+      client.release()
+  }
+  return result;
   };
 
   

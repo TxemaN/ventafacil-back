a99ca41b846f581213115d87ff782935aa7e3409
navarrorafa <115833775+navarrorafa@users.noreply.github.com>
2023-10-01 20:16:41 +0200

    Back Doc

--- models/categoryQueries.js
+++ models/categoryQueries.js
@@ -1,13 +1,39 @@
 
+/**
+ * Objeto que contiene las consultas SQL preparadas para trabajar con la tabla de categorías.
+ * @type {Object}
+ */
 const querieCats = {
+    /**
+     * Consulta que obtiene todas las categorías de la tabla.
+     * @type {string}
+     */
+    byTodas: 'SELECT * FROM categorias',
     
-    byTodas : 'SELECT * FROM categorias',
-    byId : 'SELECT * FROM categorias WHERE id_categoria = $1',
-    byNombre : 'SELECT * FROM categorias WHERE Nombre LIKE $1',
-    byInsertCat : 'INSERT INTO categorias (Nombre, descripcion) VALUES ($1, $2) RETURNING *',
+    /**
+     * Consulta que obtiene una categoría específica de la tabla usando su ID.
+     * @type {string}
+     */
+    byId: 'SELECT * FROM categorias WHERE id_categoria = $1',
+    
+    /**
+     * Consulta que obtiene categorías de la tabla basado en un patrón de nombre.
+     * @type {string}
+     */
+    byNombre: 'SELECT * FROM categorias WHERE Nombre LIKE $1',
+    
+    /**
+     * Consulta que inserta una nueva categoría en la tabla y devuelve la categoría creada.
+     * @type {string}
+     */
+    byInsertCat: 'INSERT INTO categorias (Nombre, descripcion) VALUES ($1, $2) RETURNING *',
+    
+    /**
+     * Consulta que elimina una categoría de la tabla usando su ID.
+     * @type {string}
+     */
     byBorrarUna: 'DELETE FROM categorias WHERE id_categoria = $1'
-    };
-   
+};
 
   
 
--- models/queries.js
+++ models/queries.js
@@ -1,17 +1,62 @@
+/**
+ * Objeto que contiene las consultas SQL preparadas para trabajar con la tabla de anuncios.
+ * @type {Object}
+ */
 const querieAds = {
-    
-    byAll : 'SELECT * FROM anuncios',
-    byId : 'SELECT * FROM anuncios WHERE id_anuncio = $1',
-    byName : 'SELECT * FROM anuncios WHERE Producto LIKE $1',
-    byUserName : 'SELECT * FROM anuncios WHERE Nombre_Vendedor = $1',
-    byUserID : 'SELECT * FROM anuncios WHERE id_vendedor = $1',
-    byCategoria : 'SELECT * FROM anuncios WHERE Categoria = $1',
-    byInsertQuery : 'INSERT INTO anuncios (Producto, Descripcion , Precio, Categoria, Zona_Geografica, ID_Vendedor, Ruta_foto, Precio_Stripe, Producto_Stripe, Producto_Latitude, Producto_Longitude, Nombre_Vendedor, Enlace_Pago) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *',
+    /**
+     * Consulta que obtiene todos los anuncios de la tabla.
+     * @type {string}
+     */
+    byAll: 'SELECT * FROM anuncios',
+
+    /**
+     * Consulta que obtiene un anuncio específico de la tabla usando su ID.
+     * @type {string}
+     */
+    byId: 'SELECT * FROM anuncios WHERE id_anuncio = $1',
+
+    /**
+     * Consulta que obtiene anuncios de la tabla basado en un patrón de nombre del producto.
+     * @type {string}
+     */
+    byName: 'SELECT * FROM anuncios WHERE Producto LIKE $1',
+
+    /**
+     * Consulta que obtiene anuncios de la tabla basado en el nombre del vendedor.
+     * @type {string}
+     */
+    byUserName: 'SELECT * FROM anuncios WHERE Nombre_Vendedor = $1',
+
+    /**
+     * Consulta que obtiene anuncios de la tabla basado en el ID del vendedor.
+     * @type {string}
+     */
+    byUserID: 'SELECT * FROM anuncios WHERE id_vendedor = $1',
+
+    /**
+     * Consulta que obtiene anuncios de la tabla basado en la categoría.
+     * @type {string}
+     */
+    byCategoria: 'SELECT * FROM anuncios WHERE Categoria = $1',
+
+    /**
+     * Consulta que inserta un nuevo anuncio en la tabla y devuelve el anuncio creado.
+     * @type {string}
+     */
+    byInsertQuery: 'INSERT INTO anuncios (Producto, Descripcion , Precio, Categoria, Zona_Geografica, ID_Vendedor, Ruta_foto, Precio_Stripe, Producto_Stripe, Producto_Latitude, Producto_Longitude, Nombre_Vendedor, Enlace_Pago) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *',
+
+    /**
+     * Consulta que actualiza los datos de un anuncio específico en la tabla usando su ID.
+     * @type {string}
+     */
     byActualizar: `UPDATE anuncios SET Producto=$1, Descripcion=$2 , Precio=$3, Categoria=$4, Zona_Geografica=$5, ID_Vendedor=$6, Ruta_foto=$7, Precio_Stripe=$8, Enlace_Pago=$9  WHERE id_anuncio = $10 `,
+
+    /**
+     * Consulta que elimina un anuncio de la tabla usando su ID.
+     * @type {string}
+     */
     byBorrarUna: 'DELETE FROM anuncios WHERE id_anuncio = $1'
-    };
+};
 
-  
-
-
-    module.exports = {querieAds}
+// Exportar el objeto querieAds para ser utilizado en otros módulos
+module.exports = { querieAds };
